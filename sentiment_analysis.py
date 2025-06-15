fd1cc32c1a8442770b4c97ea4d38398d6aa5932b
mddokmak <mahmoud.dokmak@epfl.ch>
2025-06-15 11:00:59 +0200

    comments

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,3 +1,8 @@
+"""
+This python script is used to preprocess financial reports, clean the text,
+and perform sentiment analysis using a pre-trained model.
+"""
+
 import pandas as pd
 import os
 import re
@@ -9,22 +14,27 @@ class ReportsProcessing:
     def __init__(self, data_path: str, clean_folder: str):
         self.data = self._load_clean_data(data_path)
         self.clean_folder = clean_folder
+        # Create folder to store the cleaned pre-process data
         os.makedirs(self.clean_folder, exist_ok=True)
         self.device = 'cuda'  # Set to 'cuda' if needed
 
     @staticmethod
     def _load_clean_data(file_path: str) -> pd.DataFrame:
+        # Load the data from a parquet file
         data = pd.read_parquet(file_path)
         data['report_date'] = pd.to_datetime(data['report_date'])
         return data
 
     def clean_reports(self):
+        """Remove URLs, mentions, hashtags symbol and special character of all reports to allow the sentiment classifier
+        to be more accurate"""
         tqdm.pandas(desc="Cleaning Reports", unit='reports')
         self.data['cleaned_text'] = self.data['text'].progress_apply(self._clean_text)
         self.data.drop(columns=['text'], inplace=True)
 
     @staticmethod
     def _clean_text(text: str) -> str:
+        """Private function called by clean_reports"""
         text = re.sub(r"^(item 7\.)\s*", "", text, flags=re.IGNORECASE)
         text = re.sub(r"\[.*?\]", " ", text)
         text = re.sub(r"http\S+", "", text)
@@ -36,16 +46,17 @@ class ReportsProcessing:
         return cleaned[:512]
 
     def reports_sentiment_analysis(self, batch_size: int = 128):
+        """Perform sentiment analysis on the cleaned reports using a pre-trained model."""
+        # Initialize Hugging Face pipeline
         sentiment_classifier = pipeline(
             "sentiment-analysis",
             model="ProsusAI/finbert",
             device=0 if self.device == 'cuda' else -1
         )
-
+        # Process reports in batches and store the results
         texts = self.data['cleaned_text'].tolist()
         sentiments = []
         sentiment_scores = []
-
         for i in tqdm(range(0, len(texts), batch_size), desc="Detecting sentiments"):
             batch = texts[i:i + batch_size]
             results = sentiment_classifier(batch, batch_size=batch_size)
@@ -53,23 +64,26 @@ class ReportsProcessing:
             sentiments.extend([r['label'] for r in results])
             sentiment_scores.extend([r['score'] for r in results])
 
+        # Add results back to the DataFrame
         self.data['sentiment'] = sentiments
         self.data['sentiment_score'] = sentiment_scores
 
         sentiment_summary = self.data[['sentiment', 'sentiment_score', 'cleaned_text']].groupby('sentiment').agg({
-            'cleaned_text': 'count',
-            'sentiment_score': 'mean'
+            'cleaned_text': 'count', # Count Reports per sentiment
+            'sentiment_score': 'mean' # Average sentiment score per sentiment
         }).rename(columns={'cleaned_text': 'report_count', 'sentiment_score': 'avg_sentiment_score'})
 
+        # Feedback to the user
         print("Sentiment Analysis Report:")
         for label in ['positive', 'neutral', 'negative']:
             if label in sentiment_summary.index:
                 count = sentiment_summary.loc[label, 'report_count']
                 avg_score = sentiment_summary.loc[label, 'avg_sentiment_score']
-                print(f"\t- {label.title()}: {count} tweets (Avg. score: {avg_score:.2f})")
+                print(f"\t- {label.title()}: {count} reports (Avg. score: {avg_score:.2f})")
             else:
-                print(f"\t- {label.title()}: 0 tweets (Avg. score: N/A)")
-
+                print(f"\t- {label.title()}: 0 reports (Avg. score: N/A)")
+                
+    """Save the cleaned data to a parquet file in the clean folder."""
     def save_clean_data(self):
         self.data.to_parquet(
             os.path.join(self.clean_folder, 'sentiment_tenkreports.parquet'), index=False
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+"""
+This python script is used to define functions for transforming and aggregating earnings calls and 10-K reports sentiment results with other predictors.
+"""
 import pandas as pd
 
+# Prepare earnings calls sentiment results
 def transform_earnings_calls(df):
     sentiment_map = {'positive': 1, 'neutral': 0, 'negative': -1}
     df['sentiment_label'] = df['sentiment'].map(sentiment_map)
@@ -8,6 +12,7 @@ def transform_earnings_calls(df):
     df['weighted_sentiment'] = df['sentiment_label'] * df['sentiment_score']
     return df
 
+# Prepare 10-K sentiment results
 def transform_tenkreports(df):
     sentiment_map = {'positive': 1, 'neutral': 0, 'negative': -1}
     df['sentiment_label'] = df['sentiment'].map(sentiment_map)
@@ -16,15 +21,17 @@ def transform_tenkreports(df):
     df['weighted_sentiment'] = df['sentiment_label'] * df['sentiment_score']
     return df
 
+# Aggregate earnings calls sentiment results
 def aggregate_earnings_calls(df):
     return df.groupby(['gvkey','mostimportantdateutc'], as_index=False).agg(
     weighted_sentiment_earnings_calls=('weighted_sentiment', 'mean'))
 
+# Aggregate 10-K sentiment results
 def aggregate_tenkreports(df):
     return df.groupby(['cik','report_date'], as_index=False).agg(
     weighted_sentiment_10k=('weighted_sentiment', 'mean'))
 
-
+# Prepare dataframes for merging with earnings calls
 def prepare_for_merge_ec(target, transcript_sentiment):
     target = target.sort_values(['MthCalDt']).reset_index(drop=True)
     transcript_sentiment = transcript_sentiment.sort_values(['mostimportantdateutc']).reset_index(drop=True)
@@ -33,6 +40,7 @@ def prepare_for_merge_ec(target, transcript_sentiment):
     target['gvkey'] = target['gvkey'].astype(int)
     return target, transcript_sentiment
 
+# Prepare dataframes for merging with 10-K reports
 def prepare_for_merge_10k(target, transcript_sentiment):
     target = target.sort_values(['MthCalDt']).reset_index(drop=True)
     transcript_sentiment = transcript_sentiment.sort_values(['report_date']).reset_index(drop=True)
@@ -41,12 +49,13 @@ def prepare_for_merge_10k(target, transcript_sentiment):
     target['cik'] = target['cik'].astype(int)
     return target, transcript_sentiment
 
-
+# Fill missing values in earnings calls sentiment results
 def fill_missing_earning_calls(df):
     df['mostimportantdateutc'] = df['mostimportantdateutc'].fillna(df['MthCalDt'])
     df['weighted_sentiment_earnings_calls'] = df['weighted_sentiment_earnings_calls'].fillna(0)
     return df
 
+# Fill missing values in 10-K reports sentiment results
 def fill_missing_10k(df):
     df['report_date'] = df['report_date'].fillna(df['MthCalDt'])
     df['weighted_sentiment_10k'] = df['weighted_sentiment_10k'].fillna(0)
